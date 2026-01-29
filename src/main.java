5efe7344ca607da734e4c36d34f8515a244500c0
daud0095 <daudibragimov2004@gmail.com>
2026-01-29 19:57:09 +0100

    Update README with author information
    Added author name and email to README.

---
+++ src/main.java
@@ -0,0 +1,11 @@
+String name = "Ragnar";
+int HealtPoints = 85;
+int maxtHealt = 100;
+int level = 5;
+int experiencePoints = 2300;
+double gold = 156.50;
+boolean isLive = true;
+
+void main(){
+
+}
