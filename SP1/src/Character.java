63fa87d607550f215d54f4e3978b596a1decec68
daud0095 <daudibragimov2004@gmail.com>
2026-02-22 22:41:37 +0100

    rettelser

--- SP1/src/Character.java
+++ SP1/src/Character.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+
 class Character {
-    int health;
-    String name;
+    private int health;
+    private String name;
     int level;
     char classType;
     int maxHealth;
     double gold;
     int xp;
+
+    ArrayList<String> inventory;
 
     Character(String name, char classType){
         this.name = name;
@@ -15,11 +19,93 @@ class Character {
         this.health = maxHealth;
         this.gold = 0;
         this.xp = 0;
+        this.inventory = new ArrayList<>();
     }
 
-    void printCharacterSheet(){
+    public void printCharacterSheet(){
+        System.out.println("\n=== CHARACTER SHEET ===");
         System.out.println("Characters name: " + name + " (" + classType + ")");
         System.out.println("Characters level: " + level + " Health: " + health + "/" + maxHealth);
+        System.out.println("XP: " + xp);
+        System.out.println("Gold: " + gold);
+    }
+
+    public void takeDamage(int amount){
+        int oldHealth = health;
+        health -= amount;
+        if (health <= 0){
+            health = 0;
+            System.out.println(name +  " is dead");
+        } else {
+            System.out.println(name + " took " + amount + " damage. Health: " + oldHealth + " -> " + health);
+        }
+    }
+
+    public void heal(int amount){
+        int oldHealth = health;
+        health += health;
+        if(health > maxHealth) {
+            health = maxHealth;
+            System.out.println(name + " heals " + amount + " HP! Health: " + oldHealth + "/" + health);
+        }
+    }
+
+    public void addGold(double amount){
+        gold += amount;
+    }
+
+    public boolean removeGold(double amount){
+        if(gold >= amount){
+            gold -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addXP(int amount){
+        xp += amount;
+        System.out.println(name + " gains " + amount + " XP Total: " + xp);
+        if(xp >= 1000){
+            levelUp();
+        }
+    }
+
+    public void levelUp(){
+        level++;
+        xp = 0;
+        maxHealth += 20;
+        health = maxHealth;
+        System.out.println(name + " leveled up! Current level is: " + level + " with max health " + maxHealth);
+    }
+
+    public boolean isHealthCritical(){
+        return health < maxHealth * 0.25;
+    }
+
+    public boolean isAlive(){
+        return health > 0;
+    }
+
+    public double getHealthPercentage(){
+        return (health * 100.0) / maxHealth;
+    }
+
+    void attack(Character enemy){
+        int damage = (int)(Math.random() * 20) + 5; // Damage er mellem 5 og 24;
+        System.out.println(name + " \nattacks " + enemy.name + " for " + damage + " damage");
+        enemy.takeDamage(damage);
+    }
+
+    public void printInventory(){
+        System.out.println("\nInventory:");
+        if(inventory.isEmpty()){
+            System.out.println("empty");
+        } else {
+            for(String invent : inventory){
+                System.out.println(" -" + invent);
+            }
+        }
     }
 
 }
--- SP1/src/Main.java
+++ SP1/src/Main.java
@@ -1,73 +1,28 @@
-String name = "King";
-int health = 85;
-int maxHealth = 100;
-int level = 5;
-int experiencePoints = 2300;
-double gold = 156.50;
-boolean isAlive = true;
-char classType = 'W';
-String[] inventory = {"Sword", "Shield", "Potion"};
-int incomingDamage = 30;
 
-void main(){
 
-    System.out.println("=== CHARACTER SHEET ===");
-    System.out.println("Name: " + name);
-    System.out.println("Class: " + classType);
-    System.out.println("Level: " + level);
-    System.out.println("Health: " + health + "/" + maxHealth);
-    System.out.println("XP: " + experiencePoints);
-    System.out.println("Gold: " + gold);
-    System.out.println("Alive: " + isAlive);
+public class Main{
+    public static void main(String[] args){
 
-    System.out.println("\nInventory (" + inventory.length + " items):");
-    for(String item : inventory) {
-        System.out.println("- " + item);
+        Character king = new Character("King", 'W');
+        Character enemy = new Character("Enemy", 'D');
+
+        king.inventory.add("Sword");
+        king.inventory.add("Shiled");
+
+        enemy.inventory.add("Axe");
+        enemy.inventory.add("Knife");
+
+        king.printCharacterSheet();
+        enemy.printInventory();
+
+        king.attack(enemy);
+        enemy.attack(king);
+
+       king.addXP(1200);
+       enemy.addXP(1000);
+
+       king.printCharacterSheet();
+       enemy.printCharacterSheet();
+
     }
-
-    System.out.println("\n=== STATUS ===");
-
-    if (health <= 0) {
-        isAlive = false;
-        System.out.println("The king has fallen!(");
-    } else if (health < maxHealth * 0.25) {
-        System.out.println("WARNING: Health critical");
-    }
-
-    switch (classType) {
-        case 'W':
-            System.out.println("You are big and strong warrior");
-            break;
-        case 'F':
-            System.out.println("You are too fast warrior");
-            break;
-        case 'R':
-            System.out.println("You are sneaky Rogue");
-            break;
-        default:
-            System.out.println("Unknown class");
-    }
-
-    int levelUpThreshold = 1000 * level;
-    if (experiencePoints >= levelUpThreshold) {
-        System.out.println("\nReady to level op");
-    }
-
-    System.out.println("\n=== COMBAT ===");
-
-    if(incomingDamage >= health) {
-        System.out.println(name + " takes " + incomingDamage + " damage!");
-        System.out.println("Health: " + health + " -> 0");
-        health = 0;
-        isAlive = false;
-        System.out.println(name + " is dead.");
-    } else {
-        System.out.println(name + " takes " + incomingDamage + " damage!");
-        System.out.println("Health: " + health + " -> " + (health - incomingDamage));
-        health -= incomingDamage;
-        isAlive = true;
-        System.out.println(name + " is still alive.");
-    }
-
-
 }
